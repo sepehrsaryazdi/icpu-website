11f11b29c9ce3227070546b8c600f4926ad98a42
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2023-02-06 20:34:31 +1100

    Added automated sticky changing

--- src/scroll_image_sequence.js
+++ src/scroll_image_sequence.js
@@ -28,6 +28,9 @@ console.log(imageSequenceControl.imageSequences)
 // var _img = document.getElementById('fixed');
 var _img = document.getElementById("scrollingImage")
 
+var _sticky = document.getElementById("test")
+_sticky.isScrolling = true;
+
 
 var newImg = new Image;
 newImg.src = dogSequenceImages[0];
@@ -47,14 +50,31 @@ window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     // console.log(scroll)
     
+    
     if(_img.style.top <= this.scrollY & this.scrollY <= _img.style.top + 10*137){
         _img.style.opacity = 1;
         newImg.src = dogSequenceImages[parseInt(this.scrollY/10)];
+        _sticky.classList.add("sticky");
+        _sticky.classList.remove("relative");
+        _sticky.style.top = 0 + "px";
+       
+        _sticky.isScrolling = true;
+        
+        
     } else {
         // setInterval(function () {_img.style.opacity -= 0.1*(_img.style.opacity)}, 1000);
         _img.style.opacity = 0;
+        _sticky.classList.remove("sticky");
+        _sticky.classList.add("relative");
+        if(_sticky.isScrolling) {
+            
+            console.log(window.innerWidth)
+        _sticky.style.top = -0.1*window.innerWidth+ this.scrollY + "px";
+        }
+        _sticky.isScrolling = false;
     }
-    
+
+
     console.log(_img.style.top)
 
     // _img.style.opacity = 1/(1+this.scrollY);
