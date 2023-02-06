7f48a118b5ca8b479bf0c252b1163c896c2eed2c
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2023-02-06 20:56:46 +1100

    Added fresh copy of everything

--- src/scroll_image_sequence.js
+++ src/scroll_image_sequence.js
@@ -5,79 +5,32 @@ for (i= 1; i < 139 ; i++) {
     dogSequenceImages.push(`src/svg_sequence/dog/dog` + `${`${i}`}`.padStart(4, '0') + `.svg`)
 }
 
-// console.log(dogSequenceImages)
 
-// console.log(dogSequenceImages)
-
-const imageSequences = [dogSequenceImages]
-
+const imageSequences = [dogSequenceImages, dogSequenceImages, dogSequenceImages]
+const divs = document.getElementsByClassName("separateSection")
+const imgs = document.getElementsByClassName("scrollingImage")
 
 
 class ImageSequenceControl {
-    constructor(imageSequences) {
+    constructor(imageSequences, divs, imgs) {
         this.imageSequences = imageSequences
+        this.divs = divs
+        this.imgs = imgs
       }
-}
 
-
-const imageSequenceControl = new ImageSequenceControl(imageSequences)
-
-console.log(imageSequenceControl.imageSequences)
-
-
-// var _img = document.getElementById('fixed');
-var _img = document.getElementById("scrollingImage")
-
-var _sticky = document.getElementById("test")
-_sticky.isScrolling = true;
-
-
-var newImg = new Image;
-newImg.src = dogSequenceImages[0];
-
-
-newImg.onload = function() {
-    _img.src = this.src;
-}
-
-// console.log(newImg)
-
-
-
-
-
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    // console.log(scroll)
-    
-    
-    if(_img.style.top <= this.scrollY & this.scrollY <= _img.style.top + 10*137){
-        _img.style.opacity = 1;
-        newImg.src = dogSequenceImages[parseInt(this.scrollY/10)];
-        _sticky.classList.add("sticky");
-        _sticky.classList.remove("relative");
-        if(!_sticky.isScrolling) {
-         _sticky.style.top = 0 + "px";
-        }
-        _sticky.isScrolling = true;
+    updateState(scroll) {
+        console.log(scroll)
         
-        
-    } else {
-        // setInterval(function () {_img.style.opacity -= 0.1*(_img.style.opacity)}, 1000);
-        _img.style.opacity = 0;
-        _sticky.classList.remove("sticky");
-        _sticky.classList.add("relative");
-        if(_sticky.isScrolling) {
-            console.log(window.innerWidth)
-        _sticky.style.top = -0.1*window.innerWidth+ this.scrollY + "px";
-        }
-        _sticky.isScrolling = false;
     }
 
 
-    console.log(_img.style.top)
+    
+}
 
-    // _img.style.opacity = 1/(1+this.scrollY);
-    // _img.style.top = this.scrollY + "px";
-    // _img.style.left = this.scrollX + "px";
-});
+
+const imageSequenceControl = new ImageSequenceControl(imageSequences, divs, imgs);
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    imageSequenceControl.updateState(scroll)  
+})
